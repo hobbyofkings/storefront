21ba01a9a8d17524b7e569841d95b7ec75ae3cc5
hobbyofkings <arnoldasnt@gmail.com>
2024-09-21 14:59:29 +0300

    Fix DATABASES configuration with explicit ENGINE

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -12,19 +12,16 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['amadesa-prod.herokuapp.com']
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
-# Explicit database engine specification along with database URL
-if DATABASE_URL:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',  # Explicitly specify PostgreSQL engine
-            **dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True),
-        }
+# Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # Explicitly set the PostgreSQL engine
+        **dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        ),
     }
-else:
-    raise ImproperlyConfigured("DATABASE_URL environment variable not set.")
-
+}
 # Redis settings
 REDIS_URL = os.environ.get('REDIS_URL')
 
