42b65c0b5439aba1e4edde54092df401d7cede25
hobbyofkings <arnoldasnt@gmail.com>
2024-09-23 11:10:05 +0300

    Postgress

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Initialize environment variables
-env = os.environ.Env(
+env = environ.Env(
     DEBUG=(bool, False)
 )
 
