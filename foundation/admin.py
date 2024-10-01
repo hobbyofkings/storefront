d44efaeebee9e298228980667598d846b2beb925
hobbyofkings <arnoldasnt@gmail.com>
2024-10-01 08:43:28 +0300

    Commit

--- foundation/admin.py
+++ foundation/admin.py
@@ -1,19 +1,30 @@
 from django.db import transaction, IntegrityError
 from django.contrib import admin
-from .models import Language, AlternativeName
+from .models import Language, AlternativeName, Country, CountryLanguage
 
+
+# Inline for managing alternative names of a language
 class AlternativeNameInline(admin.TabularInline):
     model = AlternativeName
     extra = 1
     can_delete = True
 
+
+# Inline for managing relationships between countries and languages
+class CountryLanguageInline(admin.TabularInline):
+    model = CountryLanguage
+    extra = 1
+    can_delete = True
+
+
+# Admin for managing Language with inlines for alternative names and country relationships
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ['name', 'iso2', 'iso3', 'native_name', 'created_at']
     search_fields = ['name', 'native_name', 'iso2', 'iso3']
     list_filter = ['created_at']
     list_per_page = 25
-    inlines = [AlternativeNameInline]
+    inlines = [AlternativeNameInline, CountryLanguageInline]  # Add AlternativeName and CountryLanguage inlines
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         try:
@@ -23,3 +34,12 @@ class LanguageAdmin(admin.ModelAdmin):
             self.message_user(request, f"An error occurred: {e}", level='error')
             return super().changelist_view(request, extra_context=extra_context)
 
+
+# Admin for managing Country with inline for language relationships
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['iso_name', 'official_state_name', 'iso2', 'iso3', 'created_at']
+    search_fields = ['iso_name', 'official_state_name', 'iso2', 'iso3']
+    list_filter = ['created_at']
+    ordering = ['iso_name']
+    inlines = [CountryLanguageInline]  # Add inline for managing country-language relationships
--- foundation/models.py
+++ foundation/models.py
@@ -1,8 +1,7 @@
-
 from django.db import models
 
 
-from django.db import models
+
 
 class Language(models.Model):
     name = models.CharField(max_length=100, unique=True)
